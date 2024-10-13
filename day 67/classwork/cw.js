575c5922d6bfc5378bbf966417916dd2e4cef5d8
aleqsandreapkhazava <aleqsandreapkhazava@gmail.com>
2024-10-13 13:01:41 +0400

    Goa is best frfrfrfrfrfrfrfrfrfr

---
+++ day 67/classwork/cw.js
@@ -0,0 +1,29 @@
+console.log(41**32)
+console.log(4121**323)
+console.log(121**34122)
+console.log(11**0)
+console.log(241**31)
+
+console.log(17%12)
+console.log(69%96)
+console.log(34%34)
+console.log(17%122)
+console.log(1%12)
+
+
+
+
+
+//Gift boxes
+//ვიპოვოთ მაქსიმალური რიცხვი სადამდეც იყოფა 7 უნაშთოდ
+console.log(200%196)
+
+
+
+
+
+//classwork number 2
+let balance = parseInt(readLIne(), 2)
+let withdraw = parseInt(readLine(),1) 
+
+let(balance -withdraw)
---
+++ day 67/homework/hw.js
@@ -0,0 +1,51 @@
+let a =5
+let b =5
+console.log(5**5)
+
+console.log(a>b)
+
+console.log(a++)
+
+console.log(a--)
+
+
+console.log(10+10)
+console.log(19120+4110)
+console.log(120+10)
+console.log(3110+10)
+console.log(-1+15560)
+
+
+console.log(51-11)
+console.log(1241-41)
+console.log(256-55)
+console.log(56-41)
+console.log(1-51)
+
+
+console.log(5*5)
+console.log(6*5)
+console.log(7*5)
+console.log(8*5)
+console.log(9*5)
+
+
+console.log(10/10)
+console.log(1-10/10)
+console.log(110/10)
+console.log(350/10)
+console.log(20/10)
+
+
+
+console.log(215**54)
+console.log(25**53)
+console.log(54**52)
+console.log(25**51)
+console.log(45**50)
+
+
+
+
+//solo learn
+console.log("I'm learning javascript. It's easy");
